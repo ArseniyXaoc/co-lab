2a0e81ec8d44bfe392bb089e963d4620b3690da8
ArseniyXaoc <xaoc190789@gmail.com>
2025-05-24 16:04:15 +0300

    fix deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
 import './globals.css';
-import localFont from 'next/font/local';
 import Header from '@/components/Header';
 import { ClerkProvider } from '@clerk/nextjs';
 import { AppSidebar } from '@/components/AppSidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 const RootLayout = ({
   children,
